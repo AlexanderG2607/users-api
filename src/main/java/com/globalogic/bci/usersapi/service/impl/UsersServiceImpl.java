c3fdebb0cc2ca09c256023c465c5d5aef7a82d38
Alexander Gonzalez <lex.18.1985@gmail.com>
2023-09-25 15:38:28 -0300

    signUp endopoint, email validation, password validation

---
+++ src/main/java/com/globalogic/bci/usersapi/service/impl/UsersServiceImpl.java
@@ -0,0 +1,17 @@
+package com.globalogic.bci.usersapi.service.impl;
+
+import com.globalogic.bci.usersapi.dto.CreateUserRequestDTO;
+import com.globalogic.bci.usersapi.dto.CreateUserResponseDTO;
+import com.globalogic.bci.usersapi.service.UsersService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+
+    @Override
+    public CreateUserResponseDTO signUp(CreateUserRequestDTO createUserRequestDTO) {
+        return null;
+    }
+
+}
